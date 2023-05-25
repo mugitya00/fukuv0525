c5869dde4ee407e79c3a66b93a3c86cb2a3550e6
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-05-25 13:57:16 +0900

    顔

--- fukuv0525/Form1.cs
+++ fukuv0525/Form1.cs
@@ -10,13 +10,14 @@ namespace fukuv0525
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Top = label1.Top - 15;
-            label1.Text = "(>_<)";
+            label1.Text = "(';')";
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             label1.Left = label1.Left - 15;
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
