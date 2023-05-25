61117b65b8c77e6d763832baa1d8b9e8aa3d07e2
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-05-25 13:34:44 +0900

    復習　ラベル移動

--- fukuv0525/Form1.cs
+++ fukuv0525/Form1.cs
@@ -6,5 +6,30 @@ namespace fukuv0525
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 15;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 15;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 15;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 15;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ä›ñÏóIê^");
+        }
     }
 }
